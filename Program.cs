79457bac27efddc36ee0c075a85456f3bad6b1ca
Shock Liang <shockwater@gmail.com>
2018-02-25 17:12:38 +0800

    Initital commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace math_benchmark_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
